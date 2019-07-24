22a1e80ff7a80d7387716607ba3f3046abe449cf
kittisitthichai <kittisitthichai30084@gmail.com>
2019-07-24 09:15:33 +0700

    edit projects with deploy 1

--- store/store/urls.py
+++ store/store/urls.py
@@ -11,6 +11,6 @@ router.register('customer', views.CustomerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
+    url(r'api/v1/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
