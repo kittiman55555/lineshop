5302f531a3195d5cb67083ccff947e0f73bd1f17
kittisitthichai <kittisitthichai30084@gmail.com>
2019-07-24 15:07:21 +0700

    edit no ruq img filed

--- customer/models.py
+++ customer/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    store_id = models.IntegerField(default=0)
+    store_id = models.IntegerField(default=0, blank=True)
     line_id = models.CharField(max_length=50)
-    profile_img = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    profile_img = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return self.first_name
