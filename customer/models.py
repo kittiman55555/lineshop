02b3465dcfa9334f354d4545a05a898f27f20187
kittisitthichai <kittisitthichai30084@gmail.com>
2019-07-29 12:12:56 +0700

    edit model customer

--- customer/models.py
+++ customer/models.py
@@ -4,12 +4,11 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    name_line = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    name_line = models.CharField(max_length=50, blank=True)
     store_id = models.IntegerField(default=0, blank=True)
     line_id = models.CharField(max_length=50)
-    profile_img = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return self.first_name
