6ae2a6f3093508a2af47fe97d31b9505829cf1a4
kittisitthichai <kittisitthichai30084@gmail.com>
2019-08-02 16:05:53 +0700

    5

--- store/settings.py
+++ store/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 #import django_heroku
-import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,12 +86,16 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'storelines.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'storelines.sqlite3'),
+    }
+}
+import dj_database_url
+db_from_venv = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_venv)
+
 
 # DATABASES = {
 #     'default': {
@@ -114,8 +118,6 @@ WSGI_APPLICATION = 'store.wsgi.application'
 #     }
 # }
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
